e73df3169d912f4ec99ccb11020f4e620cdbdefe
Sahilmokarram <sahilshaikh17@eng.rizvi.edu.in>
2024-11-13 09:52:57 +0530

    Add files via upload

---
+++ exp 1 task1 sahil.c
@@ -0,0 +1,16 @@
+/*Aim: WAP to print your name, age, class, division and UIN on the screen.
+ Use escape sequences to properly format the output*/
+#include<stdio.h>
+int main()
+{
+    printf("");
+    printf("roll no \t:68\n");
+    printf("div     \t:AIDS\n");
+    printf("class   \t: a\n");
+    printf("UIN     \t: 241068");
+    return 0 ;
+}
+/*roll no         :68
+div             :AIDS
+class           : a
+UIN             : 241068*/
---
+++ exp 2 task 1.c
@@ -0,0 +1,24 @@
+/*WAP to find if entered number is
+even or odd.*/
+
+#include<stdio.h>
+int main()
+{
+int num;
+printf("***EVEN ODD FINDER***\n\n");
+printf("enter number:");
+scanf("%d",&num);
+printf("\n entered number is %d\n",num);
+
+if(num%2==0)
+{
+
+
+printf("enter num is even");
+}
+ else
+ printf("enter num is odd");
+
+ return 0;
+ }
+
